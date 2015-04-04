a69dad6280405cd609fa66c81a93a66db37fc396
BakerStudio <billshark507@gmail.com>
2015-04-04 10:38:10 -0400

    First commit
    Error: increment and decrement are executed at same time

--- Click Counter/ViewController.swift
+++ Click Counter/ViewController.swift
@@ -10,15 +10,65 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var count = 0
+    var label:UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        // Create label
+        
+        var label = UILabel()
+        label.frame = CGRectMake(150, 150, 60, 60)
+        label.text = "0"
+        
+        self.view.addSubview(label)
+        self.label = label
+        
+        // Create button
+        
+        var button = UIButton()
+        button.frame = CGRectMake(150, 250, 60, 60)
+        button.setTitle("Add", forState: .Normal)
+        button.setTitleColor(UIColor.blueColor(), forState: .Normal)
+        self.view.addSubview(button)
+        
+        button.addTarget(self,          // target
+            action: "incrementCount",   // method to use
+            forControlEvents: UIControlEvents.TouchUpInside)  // control event
+        
+        // Create decrement button
+        
+        var dec = UIButton()
+        dec.frame = CGRectMake(150, 350, 60, 60)
+        dec.setTitle("Sub", forState: .Normal)
+        dec.setTitleColor(UIColor.redColor(), forState: .Normal)
+        self.view.addSubview(dec)
+
+        button.addTarget(self,          // target
+            action: "decrementCount",   // method to use
+            forControlEvents: UIControlEvents.TouchUpInside)  // control event
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func incrementCount() {
+        println("in incrementCount " + toString(count))
+        self.count++
+        self.label.text = "\(self.count)"
     }
+    
+    func decrementCount() {
+        println("in decrementCount " + toString(count))
+        self.count--
+        self.label.text = "\(self.count)"
+    }
+    
+    
+//    override func didReceiveMemoryWarning() {
+//        super.didReceiveMemoryWarning()
+//        // Dispose of any resources that can be recreated.
+//    }
 
 
 }
